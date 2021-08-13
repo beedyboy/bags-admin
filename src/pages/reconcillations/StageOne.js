7ae0886217a88307a8f53c007d518e65b8558cc9
Bolade Akinniyi <boladebode@gmail.com>
2021-08-13 10:27:36 +0100

    latest changes

--- src/pages/reconcillations/StageOne.js
+++ src/pages/reconcillations/StageOne.js
@@ -169,7 +169,7 @@ const StageOne = () => {
   const remarkBodyTemplate = (row) => {
     return (
       <React.Fragment>
-        <div className="p-text-nowrap">
+        <div className="p-text-wrap">
           {row.remarks}
         </div>
         {/* {row.remarks && row.remarks.length > 33
