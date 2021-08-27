9889dc4524c84b70ba1003b927b4c3b1e43c061e
Bolade Akinniyi <boladebode@gmail.com>
2021-08-27 11:52:53 +0100

    latest changes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -39,8 +39,9 @@ const Dashboard = () => {
     stats: totalProduct,
     getProducts,
     loading: productLoading,
+    filterProperty: productFilterProperty
   } = prodStore;
-  const { stats: totalUser, getUsers } = userStore;
+  const { stats: totalUser, getUsers, filterProperty: userFilterProperty } = userStore;
  
   useEffect(() => {
     getProducts();
@@ -51,10 +52,14 @@ const Dashboard = () => {
     setOptVal(d);
     if(d === "All") { 
       filterProperty("All", [])
+      productFilterProperty("All", [])
+      userFilterProperty("All", [])
     }  
   }
   const filterOption = () => {
     filterProperty(optVal, dashDate)
+    productFilterProperty(optVal, dashDate)
+    userFilterProperty(optVal, dashDate)
   }
   const totalOverdue = overdue && overdue.reduce((a, b) => a + b);
   const toggleStat = (e, field) => {
@@ -255,170 +260,7 @@ const Dashboard = () => {
         </div>
       </div>
      </> : null }
-      {/* 
-            <div className="p-col-12 p-md-6 p-lg-4">
-                <Panel header="Tasks" style={{ height: '100%' }}>
-                    <ul className='task-list'>
-                        <li>
-                            <Checkbox name="task" value="reports" checked={tasksCheckbox.indexOf('reports') !== -1} onChange={onTaskCheckboxChange} />
-                            <span className="task-name">Sales Reports</span>
-                            <i className="pi pi-chart-bar" />
-                        </li>
-                        <li>
-                            <Checkbox name="task" value="invoices" checked={tasksCheckbox.indexOf('invoices') !== -1} onChange={onTaskCheckboxChange} />
-                            <span className="task-name">Pay Invoices</span>
-                            <i className="pi pi-dollar" />
-                        </li>
-                        <li>
-                            <Checkbox name="task" value="dinner" checked={tasksCheckbox.indexOf('dinner') !== -1} onChange={onTaskCheckboxChange} />
-                            <span className="task-name">Dinner with Tony</span>
-                            <i className="pi pi-user" />
-                        </li>
-                        <li>
-                            <Checkbox name="task" value="meeting" checked={tasksCheckbox.indexOf('meeting') !== -1} onChange={onTaskCheckboxChange} />
-                            <span className="task-name">Client Meeting</span>
-                            <i className="pi pi-users" />
-                        </li>
-                        <li>
-                            <Checkbox name="task" value="theme" checked={tasksCheckbox.indexOf('theme') !== -1} onChange={onTaskCheckboxChange} />
-                            <span className="task-name">New Theme</span>
-                            <i className="pi pi-globe" />
-                        </li>
-                        <li>
-                            <Checkbox name="task" value="flight" checked={tasksCheckbox.indexOf('flight') !== -1} onChange={onTaskCheckboxChange} />
-                            <span className="task-name">Flight Ticket</span>
-                            <i className="pi pi-briefcase" />
-                        </li>
-                    </ul>
-                </Panel>
-            </div>
-
-            <div className="p-col-12 p-md-6 p-lg-4 p-fluid contact-form">
-                <Panel header="Contact Us">
-                    <div className="p-grid">
-                        <div className="p-col-12">
-                            <Dropdown value={dropdownCity} onChange={(e) => setDropdownCity(e.value)} options={dropdownCities} optionLabel="name" placeholder="Select a City" />
-                        </div>
-                        <div className="p-col-12">
-                            <InputText type="text" placeholder="Name" />
-                        </div>
-                        <div className="p-col-12">
-                            <InputText type="text" placeholder="Age" />
-                        </div>
-                        <div className="p-col-12">
-                            <InputText type="text" placeholder="Message" />
-                        </div>
-                        <div className="p-col-12">
-                            <Button type="button" label="Send" icon="pi pi-envelope" />
-                        </div>
-                    </div>
-                </Panel>
-            </div>
-
-            <div className="p-col-12 p-lg-4 contacts">
-                <Panel header="Contacts">
-                    <ul>
-                        <li>
-                            <button className="p-link">
-                                <img src="assets/layout/images/avatar_1.png" width="35" alt="avatar1" />
-                                <span className="name">Claire Williams</span>
-                                <span className="email">clare@primereact.com</span>
-                            </button>
-                        </li>
-                        <li>
-                            <button className="p-link">
-                                <img src="assets/layout/images/avatar_2.png" width="35" alt="avatar2" />
-                                <span className="name">Jason Dourne</span>
-                                <span className="email">jason@primereact.com</span>
-                            </button>
-                        </li>
-                        <li>
-                            <button className="p-link">
-                                <img src="assets/layout/images/avatar_3.png" width="35" alt="avatar3" />
-                                <span className="name">Jane Davidson</span>
-                                <span className="email">jane@primereact.com</span>
-                            </button>
-                        </li>
-                        <li>
-                            <button className="p-link">
-                                <img src="assets/layout/images/avatar_4.png" width="35" alt="avatar4" />
-                                <span className="name">Tony Corleone</span>
-                                <span className="email">tony@primereact.com</span>
-                            </button>
-                        </li>
-                    </ul>
-                </Panel>
-            </div>
-
-           
-            <div className="p-col-12 p-lg-6">
-                <div className="card">
-                    <Chart type="line" data={lineData} />
-                </div>
-            </div>
- 
-
-            <div className="p-col-12 p-lg-4">
-                <Panel header="Activity" style={{ height: '100%' }}>
-                    <div className="activity-header">
-                        <div className="p-grid">
-                            <div className="p-col-6">
-                                <span style={{ fontWeight: 'bold' }}>Last Activity</span>
-                                <p>Updated 1 minute ago</p>
-                            </div>
-                            <div className="p-col-6" style={{ textAlign: 'right' }}>
-                                <Button label="Refresh" icon="pi pi-refresh" />
-                            </div>
-                        </div>
-                    </div>
-
-                    <ul className="activity-list">
-                        <li>
-                            <div className="p-d-flex p-jc-between p-ai-center p-mb-3">
-                                <h5 className="activity p-m-0">Income</h5>
-                                <div className="count">$900</div>
-                            </div>
-                            <ProgressBar value={95} showValue={false} />
-                        </li>
-                        <li>
-                            <div className="p-d-flex p-jc-between p-ai-center p-mb-3">
-                                <h5 className="activity p-m-0">Tax</h5>
-                                <div className="count" style={{ backgroundColor: '#f9c851' }}>$250</div>
-                            </div>
-                            <ProgressBar value={24} showValue={false} />
-                        </li>
-                        <li>
-                            <div className="p-d-flex p-jc-between p-ai-center p-mb-3">
-                                <h5 className="activity p-m-0">Invoices</h5>
-                                <div className="count" style={{ backgroundColor: '#20d077' }}>$125</div>
-                            </div>
-                            <ProgressBar value={55} showValue={false} />
-                        </li>
-                        <li>
-                            <div className="p-d-flex p-jc-between p-ai-center p-mb-3">
-                                <h5 className="activity p-m-0">Expenses</h5>
-                                <div className="count" style={{ backgroundColor: '#f9c851' }}>$250</div>
-                            </div>
-                            <ProgressBar value={15} showValue={false} />
-                        </li>
-                        <li>
-                            <div className="p-d-flex p-jc-between p-ai-center p-mb-3">
-                                <h5 className="activity p-m-0">Bonus</h5>
-                                <div className="count" style={{ backgroundColor: '#007be5' }}>$350</div>
-                            </div>
-                            <ProgressBar value={5} showValue={false} />
-                        </li>
-                        <li>
-                            <div className="p-d-flex p-jc-between p-ai-center p-mb-3">
-                                <h5 className="activity p-m-0">Revenue</h5>
-                                <div className="count" style={{ backgroundColor: '#ef6262' }}>$500</div>
-                            </div>
-                            <ProgressBar value={25} showValue={false} />
-                        </li>
-                    </ul>
-                </Panel>
-            </div>
-        */}
+      
     </div>
     </Fragment>
   );
